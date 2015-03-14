74f36a2dd8675d194002e56312b8f10c7355cfb5
Paul Saunders <SpaceCuration@gmail.com>
2015-03-14 22:50:59 +1300

    Initial Commit

---
+++ GimmeAGUIDNow/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GimmeAGUIDNow
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Press Enter for a new GUID");
+
+            Console.WriteLine();
+
+            ConsoleKeyInfo Info;
+
+            do
+            {
+
+                Console.WriteLine(Guid.NewGuid());
+
+                Info = Console.ReadKey();
+
+            }
+            while(Info.Key == ConsoleKey.Enter);
+
+        }
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Prints GUIDs
+
+Just press Enter
